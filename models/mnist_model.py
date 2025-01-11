e3e4c2647bebf443108288666f0a9289eb092263
Mahmoud Hossam <mahmoud.hossam@gmail.com>
2025-01-11 23:26:35 +0200

    Removed un-needed imports.

--- models/mnist_model.py
+++ models/mnist_model.py
@@ -6,10 +6,6 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.optim as optim
-from torchvision import datasets, transforms
-import numpy as np
-import matplotlib.pyplot as plt
 
 class Net(nn.Module):
     def __init__(self):
